f4e0f5bd4c0b096ca39ad95118645d84dc932dbf
Siva1304 <103085451+Siva1304@users.noreply.github.com>
2023-08-06 00:08:36 +0530

    Add files via upload

---
+++ BiCycle.java
@@ -0,0 +1,16 @@
+
+public class BiCycle extends Vehicle {
+
+	public BiCycle() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Inside Bicyle constructor");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BiCycle b1 = new BiCycle();		
+		System.out.println("Inside main method");
+
+	}
+
+}
